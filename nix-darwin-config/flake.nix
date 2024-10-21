b88bf7e175a7f2c2d933e45df5b3beebdd621d95
Richard Bownes <rjbownes@gmail.com>
2024-10-21 10:09:12 +0100

    flake update

--- nix-darwin-config/flake.nix
+++ nix-darwin-config/flake.nix
@@ -43,6 +43,7 @@
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
+      nix.settings.nix-path = "nixpkgs=flake:nixpkgs";
 
       # Create /etc/zshrc that loads the nix-darwin environment.
       programs.zsh.enable = true;  # default shell on catalina
